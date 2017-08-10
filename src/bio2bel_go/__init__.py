afc52bd600a4f35c5692397d5e79e5f2508e8651
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-10 19:46:02 +0200

    Add GO annotator

---
+++ src/bio2bel_go/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from . import integrate
+from .integrate import *
+
+__all__ = (
+    integrate.__all__
+)
+
+__version__ = '0.0.1-dev'
+
+__title__ = 'bio2bel_go'
+__description__ = "A package for converting GO to BEL"
+__url__ = 'https://github.com/bio2bel/go'
+
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Charles Tapley Hoyt'

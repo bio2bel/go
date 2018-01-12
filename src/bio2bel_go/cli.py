35d424cc7549f66a47d0161cbea9eec2367d14ca
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-01-12 17:00:04 +0100

    Update CLI to write BEL

--- src/bio2bel_go/cli.py
+++ src/bio2bel_go/cli.py
@@ -5,7 +5,9 @@ import sys
 
 import click
 
+import pybel
 from bio2bel_go.enrich import upload_bel
+from bio2bel_go.manager import Manager
 from bio2bel_go.to_belns import write_belns
 
 
@@ -16,12 +18,22 @@ def main():
 
 
 @main.command()
-@click.option('-o', '--output', default=sys.stdout)
+@click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
 @click.option('-p', '--path')
 def write(output, path):
     """Populate the database"""
     write_belns(path=path, file=output)
 
+
+@main.command()
+@click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
+@click.option('-p', '--path')
+def write_bel(output, path):
+    """Writes GO as a BEL script"""
+    m = Manager()
+    m.populate(path=path)
+    graph = m.to_bel()
+    pybel.to_bel(graph, output)
 
 @main.command()
 def store():
